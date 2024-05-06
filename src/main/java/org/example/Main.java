83adf2880064ae9a98d4d0de114a3f83d7206d7f
Ereb-999 <Naofumi999@yandex.ru>
2024-05-06 17:58:37 +0400

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+
+
 public class Main {
     public static void main(String[] args) {
-
+     try{
+         int masive[] = new int[6];
+         masive[9] = 0/0;
+     } catch (ArithmeticException | ArrayIndexOutOfBoundsException E) {
+     System.out.println(E.getMessage());
+     }
     }
 }
+
